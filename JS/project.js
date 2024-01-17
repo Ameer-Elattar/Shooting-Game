203981f013264321d4cf7c32d7c6fa6f2effde5f
Ameer-Elattar <amer.alattar.ae@gmail.com>
2024-01-17 21:04:38 +0200

    fix local

--- JS/project.js
+++ JS/project.js
@@ -9,10 +9,11 @@ gameCountiner.appendChild(gameinfo);
 let score=0;
  
 for(let i=0; i<localStorage.length;i++){
+    if(localStorage.key(i).slice(0,6) != 'Player' ){
+        continue;
+    }
     if(JSON.parse(localStorage.getItem(localStorage.key(i))).playername==userNameValue){
         score=JSON.parse(localStorage.getItem(localStorage.key(i))).score;
-         
-
     }
 }
 
